0a54ed1f2c9ff27a22f10ae3b4eeb8e048a06640
DESKTOP-66GI3DC\AcePl <Aceplace1116@gmail.com>
2019-12-24 10:07:15 -0500

    Can move players around in formation.

--- PyQtGUI/OffenseLibraryGUI.py
+++ PyQtGUI/OffenseLibraryGUI.py
@@ -1,25 +1,10 @@
 from UI_OffensiveEditor import Ui_OffensiveEditor
 from PyQt5.QtWidgets import  QApplication, QMainWindow, QFrame, QHBoxLayout
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QBrush
 from PyQt5.QtCore import Qt
 import sys
 import itertools
-
-from scoutcardmaker.Offense import Subformation
-test_subformation_positions ={'L1': (-8, 0),
-            'L3': (-4, 0),
-            'L2': (4, 0),
-            'L4': (8, 0),
-            'C': (0, 0),
-            'S1': (0, 1),
-            'S2': (0, 5),
-            'S3': (0, 7),
-            'S4': (12, 0),
-            'S5': (20, 0),
-            'S6': (-20, 0)}
-test_subformation = Subformation()
-for player in test_subformation.players:
-    player.x, player.y = test_subformation_positions[player.tag]
+from scoutcardmaker.Offense import Formation, PersonnelLabelMapper
 
 
 class Canvas(QFrame):
@@ -37,25 +22,32 @@ class Canvas(QFrame):
 
     @staticmethod
     def canvas_coords_to_off_player(x, y):
-        return (x - Canvas.OFF_PLAYER_START[0]) / Canvas.HOR_YD_LEN, (y - Canvas.OFF_PLAYER_START[1]) / Canvas.VER_YD_LEN
+        return (x - Canvas.OFF_PLAYER_START[0]) // Canvas.HOR_YD_LEN, (y - Canvas.OFF_PLAYER_START[1]) // Canvas.VER_YD_LEN
+
+    @staticmethod
+    def nearest_off_player_coord(canvas_x, canvas_y):
+        return (canvas_x + Canvas.HOR_YD_LEN // 2 - Canvas.OFF_PLAYER_START[0]) // Canvas.HOR_YD_LEN, \
+               (canvas_y + Canvas.VER_YD_LEN // 2 - Canvas.OFF_PLAYER_START[1]) // Canvas.VER_YD_LEN
 
     def __init__(self):
         super().__init__()
         self.setGeometry(0,0,600,600)
-        layout = QHBoxLayout(self)
-        self.setLayout(layout)
-        self.setMinimumWidth(700)
-        self.setMinimumHeight(500)
+        self.setMinimumWidth(Canvas.TOP_LEFT[0] + Canvas.HOR_YD_LEN * 108 + 20)
+        self.setMinimumHeight(Canvas.TOP_LEFT[1] + Canvas.VER_YD_LEN * 25 + 20)
         self.setStyleSheet("background-color: white;")
-        self.setAutoFillBackground(True)
+        self.formation = Formation()
+        self.current_subformation_key = "MOF_RT"
+        self.personnel_mapper = PersonnelLabelMapper()
+        self.selected_player = None
 
     def paintEvent(self, event):
         painter = QPainter(self)
+        painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.white))
         self.draw_field(painter)
-        self.draw_subformation(painter, test_subformation)
+        self.draw_subformation(painter)
 
     def draw_field(self, painter):
-        painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
         painter.drawLine(Canvas.TOP_LEFT[0], Canvas.TOP_LEFT[1],
                          Canvas.TOP_LEFT[0], Canvas.TOP_LEFT[1] + Canvas.VER_YD_LEN * 25)
         painter.drawLine(Canvas.TOP_LEFT[0] + Canvas.HOR_YD_LEN * 108, Canvas.TOP_LEFT[1],
@@ -72,16 +64,36 @@ class Canvas(QFrame):
                              Canvas.TOP_LEFT[0] + Canvas.HOR_YD_LEN * offset,
                              Canvas.TOP_LEFT[1] + Canvas.VER_YD_LEN * 5 * row + Canvas.HASH_SIZE / 2)
 
-    def draw_subformation(self, painter, sub_formation):
-        print(sub_formation)
-        for player in sub_formation.players:
+    def draw_subformation(self, painter):
+        for player in self.formation.subformations[self.current_subformation_key].players.values():
             painter.drawEllipse(Canvas.OFF_PLAYER_START[0] + player.x * Canvas.HOR_YD_LEN - Canvas.OFF_PLAYER_SIZE[0] / 2,
                                  Canvas.OFF_PLAYER_START[1] + player.y * Canvas.VER_YD_LEN - Canvas.OFF_PLAYER_SIZE[1] / 2,
                                  Canvas.OFF_PLAYER_SIZE[0], Canvas.OFF_PLAYER_SIZE[1])
             painter.drawText(
                 Canvas.OFF_PLAYER_START[0] + player.x * Canvas.HOR_YD_LEN - Canvas.OFF_PLAYER_SIZE[0] / 2,
                 Canvas.OFF_PLAYER_START[1] + player.y * Canvas.VER_YD_LEN - Canvas.OFF_PLAYER_SIZE[1] / 2,
-                Canvas.OFF_PLAYER_SIZE[0], Canvas.OFF_PLAYER_SIZE[1], Qt.AlignCenter, player.tag)
+                Canvas.OFF_PLAYER_SIZE[0], Canvas.OFF_PLAYER_SIZE[1], Qt.AlignCenter, self.personnel_mapper.get_label(player.tag))
+
+    def mousePressEvent(self, event):
+        click_x, click_y = event.x(), event.y()
+        x, y = Canvas.nearest_off_player_coord(click_x, click_y)
+
+        for player in self.formation.subformations[self.current_subformation_key].players.values():
+            if abs(player.x - x) <= 1 and player.y == y:
+                self.selected_player = player
+                break
+
+    def mouseMoveEvent(self, event):
+        mouse_x, mouse_y= event.x(), event.y()
+        x, y = Canvas.nearest_off_player_coord(mouse_x, mouse_y)
+
+        if self.selected_player:
+            self.selected_player.x, self.selected_player.y = x, y
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.selected_player = None
+
 
 
 class OffensiveLibraryEditor(QMainWindow, Ui_OffensiveEditor):
