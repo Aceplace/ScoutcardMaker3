bb6b732c243cde287f43b0fa093cf7428a0ca133
DESKTOP-66GI3DC\AcePl <Aceplace1116@gmail.com>
2020-01-08 14:39:49 -0500

    Adding load formation functionality to defense editor.

--- PyQtGUI/DefenseLibraryGUI.py
+++ PyQtGUI/DefenseLibraryGUI.py
@@ -1,4 +1,4 @@
-from UI_OffensiveEditor import Ui_OffensiveEditor
+from UI_DefensiveEditor import Ui_DefensiveEditor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QMessageBox
 from PyQt5.QtGui import QPainter, QPen, QBrush
 from PyQt5.QtCore import Qt
@@ -7,14 +7,12 @@ import json
 import itertools
 from scoutcardmaker.Offense import Formation, OffenseLibrary
 
-
 TOP_LEFT = (50, 25)
 HOR_YD_LEN = 10
 VER_YD_LEN = 22
 HASH_SIZE = 6
-OFF_PLAYER_START = (TOP_LEFT[0] + HOR_YD_LEN * 54, TOP_LEFT[1])
+OFF_PLAYER_START = (TOP_LEFT[0] + HOR_YD_LEN * 54, TOP_LEFT[1] + VER_YD_LEN * 15)
 OFF_PLAYER_SIZE = (27, 18)
-NEXT_FIELD_OFFSET = VER_YD_LEN * 12
 
 
 def clamp(n, smallest, largest):
@@ -22,18 +20,13 @@ def clamp(n, smallest, largest):
 
 
 class DefenseFrame(QFrame):
-    @staticmethod
-    def nearest_off_player_coord(canvas_x, canvas_y, field_number):
-        return ((canvas_x + HOR_YD_LEN // 2 - OFF_PLAYER_START[0]) // HOR_YD_LEN,
-                (canvas_y + VER_YD_LEN // 2 - (OFF_PLAYER_START[1] + NEXT_FIELD_OFFSET * field_number)) // VER_YD_LEN)
-
-    def __init__(self, formation, personnel_mapper):
+    def __init__(self, subformation, personnel_mapper):
         super().__init__()
         self.setGeometry(0,0,600,600)
         self.setMinimumWidth(TOP_LEFT[0] + HOR_YD_LEN * 108 + 20)
         self.setMinimumHeight(TOP_LEFT[1] + VER_YD_LEN * 39 + 20)
         self.setStyleSheet("background-color: white;")
-        self.formation = formation
+        self.offensive_subformation = subformation
         self.can_edit = True
         self.personnel_mapper = personnel_mapper
         self.selected_player = None
@@ -42,53 +35,97 @@ class DefenseFrame(QFrame):
         painter = QPainter(self)
         painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
         painter.setBrush(QBrush(Qt.white))
-        self.draw_field(painter, 0, 'Middle of Field (Going Right)' if self.can_edit else 'Middle of Field')
-        self.draw_field(painter, 1, 'Towards Field' if self.can_edit else 'Left Hash')
-        self.draw_field(painter, 2, 'Towards Boundary' if self.can_edit else 'Right Hash')
-        self.draw_subformation(painter, 0, "MOF_RT")
-        self.draw_subformation(painter, 1, "LH_RT")
-        self.draw_subformation(painter, 2, "RH_RT")
+        self.draw_field(painter)
+        self.draw_subformation(painter)
 
-    def draw_field(self, painter, field_num, field_label):
-        top_left = (TOP_LEFT[0], TOP_LEFT[1] + NEXT_FIELD_OFFSET * field_num)
-        painter.drawLine(top_left[0], top_left[1], top_left[0], top_left[1] + VER_YD_LEN * 10)
-        painter.drawLine(top_left[0] + HOR_YD_LEN * 108, top_left[1], top_left[0] + HOR_YD_LEN * 108,
-                         top_left[1] + VER_YD_LEN * 10)
-        for row in range(3):
-            painter.drawLine(top_left[0], top_left[1] + VER_YD_LEN * 5 * row,
-                             top_left[0] + HOR_YD_LEN * 108, top_left[1] + VER_YD_LEN * 5 * row)
 
-        for combo in list(itertools.product([14, 18, 36, 72, 90, 94],[0, 1, 2])):
+    def draw_field(self, painter):
+        painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
+        painter.drawLine(TOP_LEFT[0], TOP_LEFT[1],
+                         TOP_LEFT[0], TOP_LEFT[1] + VER_YD_LEN * 25)
+        painter.drawLine(TOP_LEFT[0] + HOR_YD_LEN * 108, TOP_LEFT[1],
+                         TOP_LEFT[0] + HOR_YD_LEN * 108, TOP_LEFT[1] + VER_YD_LEN * 25)
+        for row in range(6):
+            painter.drawLine(TOP_LEFT[0], TOP_LEFT[1] + VER_YD_LEN * 5 * row,
+                             TOP_LEFT[0] + HOR_YD_LEN * 108,
+                             TOP_LEFT[1] + VER_YD_LEN * 5 * row)
+
+        for combo in list(itertools.product([14, 18, 36, 72, 90, 94], [0, 1, 2, 3, 4, 5])):
             offset = combo[0]
             row = combo[1]
-            painter.drawLine(top_left[0] + HOR_YD_LEN * offset, top_left[1] + VER_YD_LEN * 5 * row - HASH_SIZE / 2,
-                             top_left[0] + HOR_YD_LEN * offset, top_left[1] + VER_YD_LEN * 5 * row + HASH_SIZE / 2)
-
-        painter.drawText(top_left[0], top_left[1] - VER_YD_LEN, 500, 40, Qt.AlignLeft, field_label)
-
-    def draw_subformation(self, painter, field_num, hash_key):
-        off_player_start = (OFF_PLAYER_START[0], OFF_PLAYER_START[1] + NEXT_FIELD_OFFSET * field_num)
-        for player in self.formation.subformations[hash_key].players.values():
-            painter.drawEllipse(off_player_start[0] + player.x * HOR_YD_LEN - OFF_PLAYER_SIZE[0] / 2,
-                                off_player_start[1] + player.y * VER_YD_LEN - OFF_PLAYER_SIZE[1] / 2,
-                                OFF_PLAYER_SIZE[0], OFF_PLAYER_SIZE[1])
-            painter.drawText(off_player_start[0] + player.x * HOR_YD_LEN - OFF_PLAYER_SIZE[0] / 2,
-                             off_player_start[1] + player.y * VER_YD_LEN - OFF_PLAYER_SIZE[1] / 2,
-                             OFF_PLAYER_SIZE[0], OFF_PLAYER_SIZE[1], Qt.AlignCenter,
-                             self.personnel_mapper.get_label(player.tag))
+            painter.drawLine(TOP_LEFT[0] + HOR_YD_LEN * offset,
+                             TOP_LEFT[1] + VER_YD_LEN * 5 * row - HASH_SIZE / 2,
+                             TOP_LEFT[0] + HOR_YD_LEN * offset,
+                             TOP_LEFT[1] + VER_YD_LEN * 5 * row + HASH_SIZE / 2)
 
 
-class DefensiveLibraryEditor(QMainWindow, Ui_OffensiveEditor):
+    def draw_subformation(self, painter):
+        try:
+            off_player_start = (OFF_PLAYER_START[0], OFF_PLAYER_START[1])
+            for player in self.offensive_subformation.players.values():
+                painter.drawEllipse(off_player_start[0] + player.x * HOR_YD_LEN - OFF_PLAYER_SIZE[0] / 2,
+                                    off_player_start[1] + player.y * VER_YD_LEN - OFF_PLAYER_SIZE[1] / 2,
+                                    OFF_PLAYER_SIZE[0], OFF_PLAYER_SIZE[1])
+                painter.drawText(off_player_start[0] + player.x * HOR_YD_LEN - OFF_PLAYER_SIZE[0] / 2,
+                                 off_player_start[1] + player.y * VER_YD_LEN - OFF_PLAYER_SIZE[1] / 2,
+                                 OFF_PLAYER_SIZE[0], OFF_PLAYER_SIZE[1], Qt.AlignCenter,
+                                 self.personnel_mapper.get_label(player.tag))
+        except Exception as e:
+            print(str(e))
+
+
+class DefensiveLibraryEditor(QMainWindow, Ui_DefensiveEditor):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.formation_library = OffenseLibrary()
         self.selected_personnel_key = "default"
-        self.modifying_formation = Formation()
-        self.composite_formation = Formation()
-        self.formation_frame = DefenseFrame(self.modifying_formation, self.formation_library.label_mappers['default'])
-        self.scrollArea_2.setWidget(self.formation_frame)
+        self.current_hash = 'MOF'
+        self.current_offensive_formation = Formation()
+        self.current_subformation = self.current_offensive_formation.subformations['MOF_RT']
+        self.defense_frame = DefenseFrame(self.current_subformation,
+                                          self.formation_library.label_mappers['default'])
+        self.scrollArea_2.setWidget(self.defense_frame)
         self.show()
+
+        self.rb_mof.setChecked(True)
+        self.rb_mof.clicked.connect(lambda: self.handle_hash_change('MOF'))
+        self.rb_lh.clicked.connect(lambda: self.handle_hash_change('LT'))
+        self.rb_rh.clicked.connect(lambda: self.handle_hash_change('RT'))
+
+    def load_offense_library_from_dict(self, library_dict):
+        self.formation_library = OffenseLibrary.from_dict(library_dict)
+
+    def handle_hash_change(self, hash):
+        try:
+            self.current_hash = hash
+            if hash == 'MOF':
+                subformation_key = 'MOF_RT'
+            else:
+                subformation_key = 'LH_RT' if hash == 'LT' else 'RH_RT'
+            self.current_subformation = self.current_offensive_formation.subformations[subformation_key]
+            self.defense_frame.offensive_subformation = self.current_subformation
+            self.defense_frame.update()
+        except Exception as e:
+            print(str(e))
+
+    def handle_get_composite(self):
+        pass
+        # formation_name = self.edit_formation_name.text()
+        # #self.composite_formation = Formation()
+        # try:
+        #     if len(formation_name) > 0:
+        #         mof_composite = self.formation_library.get_composite_subformation('MOF', formation_name)[0]
+        #         lh_composite = self.formation_library.get_composite_subformation('LH', formation_name)[0]
+        #         rh_composite = self.formation_library.get_composite_subformation('RH', formation_name)[0]
+        #         if not mof_composite is None:
+        #             self.composite_formation.subformations['MOF_RT'].copy_from(mof_composite)
+        #             self.composite_formation.subformations['LH_RT'].copy_from(lh_composite)
+        #             self.composite_formation.subformations['RH_RT'].copy_from(rh_composite)
+        #             self.formation_frame.update()
+        # except Exception as e:
+        #     from traceback import format_exc
+        #     print(format_exc())
 
 
 if __name__ == '__main__':
@@ -97,7 +134,7 @@ if __name__ == '__main__':
 
     try:
         with open('library.json', 'r') as file:
-            window.load_library_from_dict(json.load(file))
+            window.load_offense_library_from_dict(json.load(file))
     except FileNotFoundError:
         pass
 
