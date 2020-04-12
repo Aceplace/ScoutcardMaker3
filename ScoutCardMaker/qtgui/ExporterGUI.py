28504d0773e05c40ff94a3cda7fbc80d53234372
myeiter <Aceplace1116@gmail.com>
2020-04-12 09:00:20 -0400

    Added support for last file location.

--- ScoutCardMaker/qtgui/ExporterGUI.py
+++ ScoutCardMaker/qtgui/ExporterGUI.py
@@ -10,11 +10,14 @@ from scriptexports.footballtrainerscriptexport import export_to_football_trainer
 
 
 class ExportGUI(QMainWindow, Ui_ExportGui):
-    def __init__(self):
+    def __init__(self, last_load_location, last_save_location, update_directory_location_callback):
         super().__init__()
         self.setupUi(self)
+        self.last_load_location = last_load_location
+        self.last_save_location = last_save_location
         self.formation_library = OffenseLibrary()
         self.defense_libary = DefenseLibrary()
+        self.update_directory_location_callback = update_directory_location_callback
         self.actionCreate_Scout_Cards.triggered.connect(lambda: self.handle_create_scout_cards(False, False))
         self.actionCreate_Scout_Cards_Alternating.triggered.connect(lambda: self.handle_create_scout_cards(True, False))
         self.actionOff_Create_Scout_Cards.triggered.connect(lambda: self.handle_create_scout_cards(False, True))
@@ -30,20 +33,24 @@ class ExportGUI(QMainWindow, Ui_ExportGui):
 
     def handle_create_scout_cards(self, alternating, offense):
         try:
-            file_name, _ = QFileDialog.getOpenFileName(self, 'Choose Script', 'c:\\', 'Excel File (*.xlsx)')
+            file_name, _ = QFileDialog.getOpenFileName(self, 'Choose Script', self.last_load_location, 'Excel File (*.xlsx)')
             if not file_name:
                 return
 
+            self.last_load_location = file_name
             success, script = get_script_from_excel_file(file_name, self.get_sheet_choice_callback)
 
-            file_name, _ = QFileDialog.getSaveFileName(self, 'Scout Card Filename', 'c:\\', 'Power Point (*.pptx)')
+            file_name, _ = QFileDialog.getSaveFileName(self, 'Scout Card Filename', self.last_save_location, 'Power Point (*.pptx)')
             if not file_name:
                 return
 
+            self.last_save_location = file_name
             if alternating:
                 export_to_powerpoint_alternating(file_name, script, self.formation_library, self.defense_library, offense)
             else:
                 export_to_powerpoint(file_name, script, self.formation_library, self.defense_library, offense)
+
+            self.update_directory_location_callback(self.last_load_location, self.last_save_location)
 
         except Exception:
             import traceback
@@ -51,17 +58,21 @@ class ExportGUI(QMainWindow, Ui_ExportGui):
 
     def handle_create_football_trainer_script(self, alternating):
         try:
-            file_name, _ = QFileDialog.getOpenFileName(self, 'Choose Script', 'c:\\', 'Excel File (*.xlsx)')
+            file_name, _ = QFileDialog.getOpenFileName(self, 'Choose Script', self.last_load_location, 'Excel File (*.xlsx)')
             if not file_name:
                 return
 
+            self.last_load_location = file_name
             success, script = get_script_from_excel_file(file_name, self.get_sheet_choice_callback)
 
-            file_name, _ = QFileDialog.getSaveFileName(self, 'Trainer Script', 'c:\\', 'JSON (*.json)')
+            file_name, _ = QFileDialog.getSaveFileName(self, 'Trainer Script', self.last_save_location, 'JSON (*.json)')
             if not file_name:
                 return
 
+            self.last_save_location = file_name
             export_to_football_trainer(file_name, script, self.formation_library, self.defense_library)
+
+            self.update_directory_location_callback(self.last_load_location, self.last_save_location)
 
         except Exception:
             import traceback
@@ -95,9 +106,9 @@ class ChooseSheetDialog(QDialog):
         self.accept()
 
 
-def launch():
+def launch(last_load_location, last_save_location, update_last_directory_callback):
     app = QApplication(sys.argv)
-    window = ExportGUI()
+    window = ExportGUI(last_load_location, last_save_location, update_last_directory_callback)
     window.setWindowTitle('Script Exporter')
 
     try:
--- ScoutCardMaker/quickstart.py
+++ ScoutCardMaker/quickstart.py
@@ -3,6 +3,14 @@ from qtgui import OffenseLibraryGUI, DefenseLibraryGUI, CompositeViewer, Exporte
 
 argument = sys.argv[1]
 
+with open('last_directory_locations.txt', 'r') as file:
+    last_load_location = file.readline()
+    last_save_location = file.readline()
+
+def new_last_directory_callback(last_load_location, last_save_location):
+    with open('last_directory_locations.txt', 'w') as file:
+        file.writelines((last_load_location, '\n', last_save_location))
+
 if argument == 'offensegui':
     OffenseLibraryGUI.launch()
 elif argument == 'defensegui':
@@ -10,4 +18,4 @@ elif argument == 'defensegui':
 elif argument == 'compositeviewer':
     CompositeViewer.launch()
 elif argument == 'exportergui':
-    ExporterGUI.launch()
+    ExporterGUI.launch(last_load_location, last_save_location, new_last_directory_callback)
